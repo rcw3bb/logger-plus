675e63dfd06024e781740fa98092da1d22c0077a
Ron Webb <ron@ronella.xyz>
2022-05-12 13:32:31 +1200

    Clean module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
+/**
+ * The module definition for logger-plus
+ */
 module xyz.ronella.logging.logger.plus {
-    requires org.apache.logging.log4j;
-    requires org.apache.logging.log4j.core;
     requires org.slf4j;
     requires java.scripting;
 
