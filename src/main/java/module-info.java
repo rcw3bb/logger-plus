66a4c0644e24129e49c435e015f45b38399a8931
Ron Webb <ron@ronella.xyz>
2022-12-08 16:32:38 +1300

    Make slf4j transitive dependency.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@
  * The module definition for logger-plus
  */
 module xyz.ronella.logging.logger.plus {
-    requires org.slf4j;
+    requires transitive org.slf4j;
+
     requires java.scripting;
 
     exports xyz.ronella.logging;
